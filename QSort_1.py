748227a7f4c83adc47feeea3ce8ede071cd232f7
Cojense4 <cojense4@gmail.com>
2024-02-27 12:28:39 -0600

    Create QSort_1.py

---
+++ QSort_1.py
@@ -0,0 +1,49 @@
+def QS(NList):
+    def MO3(Ls):
+        F = Ls[0]
+        M = Ls[len(Ls) // 2]
+        L = Ls[-1]
+
+        if F < M < L:
+            return M
+        elif L < M < F:
+            return M
+        elif F < L < M:
+            return L
+        elif M < L < F:
+            return L
+        elif M < F < L:
+            return F
+        else:
+            return F
+
+    def IS(Ls=NList):
+        if len(Ls) <= 1:
+            return Ls
+        for i in range(1, len(Ls)):
+            nextVal = Ls[i]
+            j = i - 1
+            while j >= 0 and nextVal < Ls[j]:
+                Ls[j+1] = Ls[j]
+                j -= 1
+            Ls[j+1] = nextVal
+        return Ls
+
+    if len(NList) <= 4:
+        return IS(NList)
+
+    pivot = MO3(Ls=NList)
+
+    L, R = 0, len(NList)-1
+    while NList[L] < pivot:
+        L += 1
+    while NList[R] < pivot:
+        R -= 1
+    NList[L], NList[R] = NList[R], NList[L]
+    return QS(NList)
+
+
+nList = [5, 2, 4, 7, 8, 3, 9, 6, 1]
+print(nList)
+ans = QS(nList)
+print(ans)
