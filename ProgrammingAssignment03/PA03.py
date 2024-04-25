41c14d26fcebfc1b651b6db7ddba72235a6bceb6
Connor Jensen <cjensen32@unomaha.edu>
2024-04-25 11:40:33 -0500

    V6.2
    Description: Added working versions of algorithms, have to change algorithms to work inside description on .pdf file and in context of the class

--- ProgrammingAssignment03/PA03.py
+++ ProgrammingAssignment03/PA03.py
@@ -1,41 +1,71 @@
 from random import randint
-import time
+from time import time_ns
 
 
-def n_squared_algorithm(n_array_A, k_twinSum_value, cur_time):
-    pass
+def nSquared(nArray, kValue, curTime):
+    for i in range(len(nArray)):
+        for j in range(i+1, len(nArray)):
+            if nArray[i] + nArray[j] == kValue:
+                return [True, f'({nArray[i]} + {nArray[j]})'], time_ns() - curTime
+    return False, time_ns() - curTime
 
 
-def n_log_n_algorithm(n_array_A, k_twinSum_value, cur_time):
-    pass
+def nlogn(nArray, kValue, curTime):
+    nArray.sort()  # Sort the array in ascending order
+    left = 0
+    right = len(nArray) - 1
+    while left < right:
+        currentSum = nArray[left] + nArray[right]
+        if currentSum == kValue:
+            return [True, f'({nArray[left]} + {nArray[right]})'], time_ns() - curTime
+        elif currentSum < kValue:
+            left += 1
+        else:
+            right -= 1
+    return False, time_ns() - curTime
 
 
-def n_algorithm(n_array_A, k_twinSum_value, cur_time):
-    pass
+def n(nArray, kValue, curTime):
+    num_dict = {}
+    for i in range(len(nArray)):
+        complement = kValue - nArray[i]
+        if complement in num_dict:
+            return [True, f'({nArray[i]} + {complement})'], time_ns() - curTime
+        num_dict[nArray[i]] = i
+    return False, time_ns() - curTime
 
-def main():
-    n = int(input("Enter size of random array: "))
+def main(n):
     if n < 50:
-        n_array_A = []
+        nArray = []
         for i in range(n):
-            n_array_A.append(randint(-99, 99))
-        print(n_array_A)
+            nArray.append(randint(-99, 99))
+        print(nArray)
     else:
-        n_array_A = []
+        nArray = []
         for i in range(n):
-            n_array_A.append(randint(-99, 99))
-    k_TwinSum_value = int(input("Enter the K value: "))
-    return n_array_A, k_TwinSum_value
+            nArray.append(randint(-99, 99))
+    kValue = int(input("Enter the K value: "))
+    return nArray, kValue
 
 
 if __name__ == '__main__':
-    cur_time = time.time()
-    A, K = main()
-    print("Running the alorithms ...")
-    cur_time = time.time()
-    n_squared_result, n_squared_time = n_squared_algorithm(A, K, cur_time)
-    cur_time = time.time()
-    n_log_n_result, n_log_n_time = n_log_n_algorithm(A, K, cur_time)
-    cur_time = time.time()
-    n_result, n_time = n_algorithm(A, K, cur_time)
-    print(A, "\n", K)
+    nSize = int(input('Enter size of random array (0 to exit): '))
+    while nSize:
+        A, K = main(nSize)
+        print("Running the algorithms...")
+        for i in range(3):
+            if i == 0:
+                result, compTime = nSquared(A, K, time_ns())
+                print(f'\n<O(N**2) Algorithm>')
+            elif i == 1:
+                result, compTime = nlogn(A, K, time_ns())
+                print(f'\n<O(NlogN) Algorithm>')
+            else:
+                result, compTime = n(A, K, time_ns())
+                print(f'\n<O(N) Algorithm>')
+            if result[0]:
+                print('\tYes, there are two numbers whose sum equals to K')
+                print(f'\tK={K}, {result[1]}')
+            else:
+                print(f'\tNo, there are no two numbers whose sum equals to K')
+            print(f'\tTime taken: {compTime} ns')
